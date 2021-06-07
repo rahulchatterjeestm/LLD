f260202d586b13cfd5c556b197432a3f8bbecc3c
Rahul Kumar <rahul.raths@gmail.com>
2021-06-08 02:25:19 +0530

    added car and cmd model

---
+++ ParkingLot/src/ParkingLot/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace ParkingLot.Models
+{
+    public class Car
+    {
+        public string RegisterationNumber { get; }
+        public string Color { get; }
+
+        public Car(string registerationNumber, string color)
+        {
+            this.RegisterationNumber = registerationNumber;
+            this.Color = color;
+        }
+
+    }
+}
---
+++ ParkingLot/src/ParkingLot/Models/Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot.Models
+{
+    public class Command
+    {
+        public string CommandName { get; }
+        public List<string> Params { get; }
+
+        public Command(string inputLine)
+        {
+            List<string> tokenList = inputLine.Split().Where(i => i.Length>0).ToList();
+            if(tokenList.Count() == 0)
+            {
+                throw new Exception(); //invalid cmd
+            }
+
+            this.CommandName = tokenList[0];
+            tokenList.RemoveAt(0);
+            this.Params = tokenList;
+        }
+    }
+}
