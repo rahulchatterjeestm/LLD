7f4d39673f461de4f3b7aab20d554380a0b486fc
Rahul Chatterjee <rahulchatterjeestm@gmail.com>
2021-06-18 02:53:47 +0530

    Added Parking lot service class

---
+++ ParkingLot/src/ParkingLot/Service/ParkingLotService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParkingLot.Exceptions;
+using ParkingLot.Models;
+using ParkingLot.Models.Strategy;
+
+namespace ParkingLot.Service
+{
+    public class ParkingLotService
+    {
+        private Models.ParkingLot parkingLot;
+        private IParkingStrategy parkingStrategy;
+
+        public void CreateParkingLot(Models.ParkingLot parkingLot, IParkingStrategy parkingStrategy)
+        {
+            if(this.parkingLot != null)
+            {
+                throw new ParkingSlotException("Parking lot already exists");
+            }
+
+            this.parkingLot = parkingLot;
+            this.parkingStrategy = parkingStrategy;
+            for(int i=1; i<=parkingLot.Capacity; i++)
+            {
+                this.parkingStrategy.AddSlot(i);
+            }
+        }
+
+    }
+}
